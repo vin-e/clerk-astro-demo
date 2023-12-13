123e7f7a782431618db047c5b01a35a1451c81c0
Vincent Chavez <vincent@vin-e.com>
2023-12-13 03:55:47 -0800

    add astro vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: vercel()
 });
